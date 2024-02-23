f7aa6ebdde289e432ad02b2efff233a4dac306fb
cs0csi <cs0csi@proton.me>
2024-02-23 14:06:24 +0100

    adding comments

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import random
 from dishes_data import dishes
+
+# User's available ingredients
 available_ingredients = set()
 
 # Set to store rejected ingredients for each dish
@@ -11,6 +13,8 @@ excluded_categories = set()
 
 def get_recommendation():
     global excluded_categories
+
+    # Filter dishes based on available ingredients and excluded categories
     dish_options = [dish for dish in dishes.keys() if not any(
         ingredient in rejected_ingredients[dish] for ingredient in dishes[dish]['ingredients']) and dishes[dish]['category'] not in excluded_categories]
 
@@ -18,17 +22,21 @@ def get_recommendation():
         print("Sorry, no suitable dishes available based on your current restrictions.")
         return
 
+    # Randomly choose a dish from the available options
     dish = random.choice(dish_options)
     print(f"Recommended dish: {dish}")
 
+    # Display ingredients for the recommended dish
     ingredients = show_ingredients_info(dish)
     print(f"\nIngredients for {dish}:")
     for i, ingredient in enumerate(ingredients, start=1):
         print(f"- {ingredient}")
 
+    # Prompt user for suitability
     response = input("Is this suitable? (yes/no): ").lower()
 
     if response == "no":
+        # Provide reasons for user rejection
         reason_options = [
             f"I would not like {dishes[dish]['category']} food now",
             f"There are no ingredients at home for the suggested {
@@ -39,6 +47,7 @@ def get_recommendation():
         for i, reason in enumerate(reason_options, start=1):
             print(f"{i}. {reason}")
 
+        # Prompt user to choose a reason and handle accordingly
         reason_index = int(
             input("Choose a reason by entering its number (1, 2, etc.): ")) - 1
         reason = reason_options[reason_index]
@@ -59,16 +68,21 @@ def update_rejected_ingredients(dish):
 
     all_ingredients = dishes[dish]['ingredients']
 
+    # Display ingredients and prompt user to select rejected ones
     ingredients = show_ingredients_info(dish)
     print(f"\nIngredients for {dish}:")
     for i, ingredient in enumerate(ingredients, start=1):
         print(f"[{i}] {ingredient}")
 
+    # Prompt user to enter numbers of rejected ingredients
     rejected_indices_str = input(
         "Enter the numbers of ingredients not at home (comma-separated): ")
+
+    # Convert user input into a list of indices
     rejected_indices = [
         int(index) - 1 for index in rejected_indices_str.split(",")]
 
+    # Update rejected ingredients for the chosen dish
     rejected_ingredients[dish].update(
         all_ingredients[index] for index in rejected_indices)
 
