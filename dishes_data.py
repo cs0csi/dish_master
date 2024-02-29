5301a21805acb2931385663a6cf4ab2e662ee5d1
cs0csi <cs0csi@proton.me>
2024-02-29 17:25:44 +0100

    show recipe after chose

--- dishes_data.py
+++ dishes_data.py
@@ -3,84 +3,98 @@ dishes = {
         "ingredients": ["pasta", "minced meat", "tomato sauce", "onion", "garlic"],
         "category": "savory",
         "type": "main",
-        "preparation_time": 30  # in minutes
+        "preparation_time": 30,
+        "recipe": "Add minced meat to a hot pan, sauté until brown. Add chopped onions and garlic. Pour in tomato sauce and let it simmer. Cook pasta separately and combine with the sauce. Serve hot."
     },
     "Chicken Stir-Fry": {
         "ingredients": ["chicken", "vegetables", "soy sauce", "ginger"],
         "category": "savory",
         "type": "main",
-        "preparation_time": 20  # in minutes
+        "preparation_time": 20,
+        "recipe": "Add minced meat to a hot pan, sauté until brown. Add chopped onions and garlic. Pour in tomato sauce and let it simmer. Cook pasta separately and combine with the sauce. Serve hot."
     },
     "Chocolate Cake": {
         "ingredients": ["flour", "sugar", "chocolate", "eggs", "butter"],
         "category": "sweet",
         "type": "main",
-        "preparation_time": 45  # in minutes
+        "preparation_time": 45,
+        "recipe": "Add minced meat to a hot pan, sauté until brown. Add chopped onions and garlic. Pour in tomato sauce and let it simmer. Cook pasta separately and combine with the sauce. Serve hot."
     },
     "Caesar Salad": {
         "ingredients": ["lettuce", "chicken", "croutons", "parmesan", "Caesar dressing"],
         "category": "savory",
         "type": "main",
-        "preparation_time": 15  # in minutes
+        "preparation_time": 15,
+        "recipe": "Add minced meat to a hot pan, sauté until brown. Add chopped onions and garlic. Pour in tomato sauce and let it simmer. Cook pasta separately and combine with the sauce. Serve hot."
     },
     "Fruit Salad": {
         "ingredients": ["apple", "banana", "grapes", "orange", "honey"],
         "category": "sweet",
         "type": "main",
-        "preparation_time": 10  # in minutes
+        "preparation_time": 10,
+        "recipe": "Add minced meat to a hot pan, sauté until brown. Add chopped onions and garlic. Pour in tomato sauce and let it simmer. Cook pasta separately and combine with the sauce. Serve hot."
     },
     "Grilled Salmon": {
         "ingredients": ["salmon", "lemon", "olive oil", "herbs", "garlic"],
         "category": "savory",
         "type": "main",
-        "preparation_time": 25  # in minutes
+        "preparation_time": 25,
+        "recipe": "Add minced meat to a hot pan, sauté until brown. Add chopped onions and garlic. Pour in tomato sauce and let it simmer. Cook pasta separately and combine with the sauce. Serve hot."
     },
     "Pancakes": {
         "ingredients": ["flour", "milk", "eggs", "baking powder", "maple syrup"],
         "category": "sweet",
         "type": "main",
-        "preparation_time": 30  # in minutes
+        "preparation_time": 30,
+        "recipe": "Add minced meat to a hot pan, sauté until brown. Add chopped onions and garlic. Pour in tomato sauce and let it simmer. Cook pasta separately and combine with the sauce. Serve hot."
     },
     "Mushroom Risotto": {
         "ingredients": ["arborio rice", "mushrooms", "onion", "white wine", "parmesan"],
         "category": "savory",
         "type": "main",
-        "preparation_time": 40  # in minutes
+        "preparation_time": 40,
+        "recipe": "Add minced meat to a hot pan, sauté until brown. Add chopped onions and garlic. Pour in tomato sauce and let it simmer. Cook pasta separately and combine with the sauce. Serve hot."
     },
     "Cheesecake": {
         "ingredients": ["cream cheese", "sugar", "eggs", "vanilla extract", "graham cracker crust"],
         "category": "sweet",
         "type": "main",
-        "preparation_time": 50  # in minutes
+        "preparation_time": 50,
+        "recipe": "Add minced meat to a hot pan, sauté until brown. Add chopped onions and garlic. Pour in tomato sauce and let it simmer. Cook pasta separately and combine with the sauce. Serve hot."
     },
     "Caprese Salad": {
         "ingredients": ["tomatoes", "mozzarella", "basil", "balsamic glaze", "olive oil"],
         "category": "savory",
         "type": "main",
-        "preparation_time": 50  # in minutes
+        "preparation_time": 50,
+        "recipe": "Add minced meat to a hot pan, sauté until brown. Add chopped onions and garlic. Pour in tomato sauce and let it simmer. Cook pasta separately and combine with the sauce. Serve hot."
     },
     "Rise": {
         "ingredients": ["rise", "oil"],
         "category": "savory",
         "type": "side_dish",
-        "preparation_time": 50  # in minutes
+        "preparation_time": 50,
+        "recipe": "Add minced meat to a hot pan, sauté until brown. Add chopped onions and garlic. Pour in tomato sauce and let it simmer. Cook pasta separately and combine with the sauce. Serve hot."
     },
     "Potato": {
         "ingredients": ["rise", "oil"],
         "category": "savory",
         "type": "side_dish",
-        "preparation_time": 50  # in minutes
+        "preparation_time": 50,
+        "recipe": "Add minced meat to a hot pan, sauté until brown. Add chopped onions and garlic. Pour in tomato sauce and let it simmer. Cook pasta separately and combine with the sauce. Serve hot."
     },
     "Soup_test1": {
         "ingredients": ["rise", "oil"],
         "category": "savory",
         "type": "soup",
-        "preparation_time": 50  # in minutes
+        "preparation_time": 50,
+        "recipe": "Add minced meat to a hot pan, sauté until brown. Add chopped onions and garlic. Pour in tomato sauce and let it simmer. Cook pasta separately and combine with the sauce. Serve hot."
     },
     "Soup_test2": {
         "ingredients": ["rise", "oil"],
         "category": "savory",
         "type": "soup",
-        "preparation_time": 50  # in minutes
+        "preparation_time": 50,
+        "recipe": "Add minced meat to a hot pan, sauté until brown. Add chopped onions and garlic. Pour in tomato sauce and let it simmer. Cook pasta separately and combine with the sauce. Serve hot."
     }
 }
--- main.py
+++ main.py
@@ -67,36 +67,147 @@ def get_recommendation():
     # Inside get_recommendation() function, after asking if the dish is suitable
     elif response == "yes":
         response_side = input("Do you want any side dish? (yes/no): ").lower()
+        side_dish = None
+
         if response_side == "yes":
             # Filter available side dishes
-            side_dishes = [dish for dish in dishes.keys(
-            ) if dishes[dish]['type'] == 'side_dish']
+            side_dishes = [d for d in dishes.keys() if dishes[d]
+                           ['type'] == 'side_dish']
 
             if side_dishes:
                 print("\nAvailable Side Dishes:")
-                for i, side_dish in enumerate(side_dishes, start=1):
-                    print(f"{i}. {side_dish}")
+                for i, s_dish in enumerate(side_dishes, start=1):
+                    print(f"{i}. {s_dish}")
 
                 chosen_side_dish = input(
                     "Choose a side dish (enter the number): ")
-                print(f"You chose {
-                      side_dishes[int(chosen_side_dish) - 1]} as a side dish.")
+                side_dish = side_dishes[int(chosen_side_dish) - 1]
+                print(f"You chose {side_dish} as a side dish.")
             else:
                 print("No side dishes available.")
 
         response_soup = input("Do you want any soup? (yes/no): ").lower()
+        soup = None
+
         if response_soup == "yes":
             # List all soups and let the user choose
-            soups = [dish for dish in dishes.keys() if dishes[dish]
-                     ['type'] == 'soup']
+            soups = [d for d in dishes.keys() if dishes[d]['type'] == 'soup']
             print("\nAvailable Soups:")
-            for i, soup in enumerate(soups, start=1):
-                print(f"{i}. {soup}")
+            for i, s in enumerate(soups, start=1):
+                print(f"{i}. {s}")
 
             chosen_soup = input("Choose a soup (enter the number): ")
-            print(f"You chose {soups[int(chosen_soup) - 1]} as a soup.")
+            soup = soups[int(chosen_soup) - 1]
+            print(f"You chose {soup} as a soup.")
 
-# Rest of the code remains unchanged...
+        print(f"\nRecipe for {dish}:")
+        print(dishes[dish]['recipe'])
+
+        if side_dish:
+            print(f"\nRecipe for {side_dish}:")
+            print(dishes[side_dish]['recipe'])
+
+        if soup:
+            print(f"\nRecipe for {soup}:")
+            print(dishes[soup]['recipe'])
+
+
+def get_recommendation():
+    global excluded_categories
+    global max_preparation_time
+
+    main_dish_options = [dish for dish in dishes.keys() if
+                         dishes[dish]['type'] == 'main' and
+                         not any(ingredient in rejected_ingredients[dish] for ingredient in dishes[dish]['ingredients']) and
+                         dishes[dish]['category'] not in excluded_categories and
+                         (max_preparation_time is None or dishes[dish]['preparation_time'] <= max_preparation_time)]
+
+    if not main_dish_options:
+        print(
+            "Sorry, no suitable main dishes available based on your current restrictions.")
+        return
+
+    dish = random.choice(main_dish_options)
+    print(f"Recommended main dish: {dish}")
+
+    ingredients = show_ingredients_info(dish)
+    print(f"\nIngredients for {dish}:")
+    for i, ingredient in enumerate(ingredients, start=1):
+        print(f"- {ingredient}")
+
+    print(f"Preparation time: {dishes[dish]['preparation_time']} minutes")
+
+    response = input("Is this suitable? (yes/no): ").lower()
+
+    if response == "no":
+        reason_options = [
+            f"I would not like {dishes[dish]['category']} food now",
+            f"There are no ingredients at home for the suggested {
+                dishes[dish]['category']} dish",
+            f"I don't have that much time",
+        ]
+
+        print("\nReason options:")
+        for i, reason in enumerate(reason_options, start=1):
+            print(f"{i}. {reason}")
+
+        reason_index = int(
+            input("Choose a reason by entering its number (1, 2, etc.): ")) - 1
+        reason = reason_options[reason_index]
+
+        if reason == f"There are no ingredients at home for the suggested {dishes[dish]['category']} dish":
+            update_rejected_ingredients(dish)
+        elif reason == f"I don't have that much time":
+            max_preparation_time = int(
+                input("Enter the maximum preparation time you have (in minutes): "))
+        elif reason == f"I would not like {dishes[dish]['category']} food now":
+            excluded_categories.add(dishes[dish]['category'])
+    # Inside get_recommendation() function, after asking if the dish is suitable
+    elif response == "yes":
+        response_side = input("Do you want any side dish? (yes/no): ").lower()
+        side_dish = None
+
+        if response_side == "yes":
+            # Filter available side dishes
+            side_dishes = [d for d in dishes.keys() if dishes[d]
+                           ['type'] == 'side_dish']
+
+            if side_dishes:
+                print("\nAvailable Side Dishes:")
+                for i, s_dish in enumerate(side_dishes, start=1):
+                    print(f"{i}. {s_dish}")
+
+                chosen_side_dish = input(
+                    "Choose a side dish (enter the number): ")
+                side_dish = side_dishes[int(chosen_side_dish) - 1]
+                print(f"You chose {side_dish} as a side dish.")
+            else:
+                print("No side dishes available.")
+
+        response_soup = input("Do you want any soup? (yes/no): ").lower()
+        soup = None
+
+        if response_soup == "yes":
+            # List all soups and let the user choose
+            soups = [d for d in dishes.keys() if dishes[d]['type'] == 'soup']
+            print("\nAvailable Soups:")
+            for i, s in enumerate(soups, start=1):
+                print(f"{i}. {s}")
+
+            chosen_soup = input("Choose a soup (enter the number): ")
+            soup = soups[int(chosen_soup) - 1]
+            print(f"You chose {soup} as a soup.")
+
+        print(f"\nRecipe for {dish}:")
+        print(dishes[dish]['recipe'])
+
+        if side_dish:
+            print(f"\nRecipe for {side_dish}:")
+            print(dishes[side_dish]['recipe'])
+
+        if soup:
+            print(f"\nRecipe for {soup}:")
+            print(dishes[soup]['recipe'])
 
 
 def show_ingredients_info(dish):
